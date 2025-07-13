ac29873e55b2e93ca82a6cf0035378938cfd529d
Osmane97 <oboukara97@example.com>
2025-07-13 20:28:37 +0100

    change local host to 127.0.0.1

--- scrapers/Amazon.py
+++ scrapers/Amazon.py
@@ -246,7 +246,7 @@ print(all_products)
 
 #connect to mysql database
 connection = mysql.connector.connect(
-    host='localhost',
+    host='127.0.0.1',
     port = 3306,
     user="root",
     password="root",
